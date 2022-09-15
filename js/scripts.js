f681634ab5af62b8f7ce77241fddbce396a09133
Noah Cowan <nsncowan@gmail.com>
2022-09-15 11:36:00 -0700

    Fix inentation

--- js/scripts.js
+++ js/scripts.js
@@ -10,33 +10,33 @@ const message3 = "won't you be my neighbor?"
 
 
 function mainCount(inputNumber) {
-    let countArray = [];
-    for (let count = 0; count <= inputNumber; count++) {
-        if(count.toString().includes(num3)) {
-            countArray.push(message3);
-        }
-        else if(count.toString().includes(num2)) {
-            countArray.push(message2);
-        }
-        else if(count.toString().includes(num1)) {
-            countArray.push(message1);
-        }
-        else {
-            countArray.push(count.toString());
-        } 
+  let countArray = [];
+  for (let count = 0; count <= inputNumber; count++) {
+    if(count.toString().includes(num3)) {
+      countArray.push(message3);
     }
-    return countArray.join("\n");
+    else if(count.toString().includes(num2)) {
+      countArray.push(message2);
+    }
+    else if(count.toString().includes(num1)) {
+      countArray.push(message1);
+    }
+    else {
+      countArray.push(count.toString());
+    } 
+  }
+  return countArray.join("\n");
 }
     
 // UI Logic
 function handleFormSubmission(e) {
-    e.preventDefault();
-    const userInput = parseInt(document.querySelector("input#number").value);
-    let output = mainCount(userInput);
-    document.getElementById("output").innerText = output;
-    document.getElementById("getNumber").reset();
+  e.preventDefault();
+  const userInput = parseInt(document.querySelector("input#number").value);
+  let output = mainCount(userInput);
+  document.getElementById("output").innerText = output;
+  document.getElementById("getNumber").reset();
 }
 
 window.addEventListener("load", function() {
-    document.querySelector("form#getNumber").addEventListener("submit", handleFormSubmission);
+  document.querySelector("form#getNumber").addEventListener("submit", handleFormSubmission);
 });
